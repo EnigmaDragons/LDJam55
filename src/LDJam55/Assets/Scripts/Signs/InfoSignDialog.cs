13ec33d6d36772d6f920c2f691b26828d0242e68
Daniel Xie <danielxie@Daniels-MacBook-Pro.local>
2024-04-13 15:09:58 -0700

    Small naming refactor in InfoSignDialog

--- src/LDJam55/Assets/Scripts/Signs/InfoSignDialog.cs
+++ src/LDJam55/Assets/Scripts/Signs/InfoSignDialog.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class InfoSignDialog : OnMessage<ShowInfoSignDialog, HideInfoSignDialog>
 {
-    [SerializeField] private GameObject dialogCanvas = null;
+    [SerializeField] private GameObject dialogPanel = null;
     [SerializeField] private TMPro.TextMeshProUGUI text = null;
 
     protected override void Execute(ShowInfoSignDialog msg) {
         text.text = msg.text;
-        dialogCanvas.SetActive(true);
+        dialogPanel.SetActive(true);
     }
 
     protected override void Execute(HideInfoSignDialog msg) {
-        dialogCanvas.SetActive(false);
+        dialogPanel.SetActive(false);
     }
 }
