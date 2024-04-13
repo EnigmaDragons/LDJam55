e493010721aad862c93064554b9c5be57c06a649
Quinn Belcastro <Hardly.Quinn@outlook.com>
2024-04-13 16:27:44 -0700

    Summon ctrl now only shows when you have summons

--- src/LDJam55/Assets/Scripts/Summons/SummonMenu.cs
+++ src/LDJam55/Assets/Scripts/Summons/SummonMenu.cs
@@ -1,4 +1,5 @@
 ﻿
+using Sirenix.Utilities;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -10,25 +11,32 @@ public class SummonMenu : OnMessage<GameStateChanged, HideSummonMenu, ShowSummon
     [SerializeField] private GameObject summonDaddy;
     [SerializeField] private GameObject[] thingsToDisable;
 
-    Dictionary<string, SummonUI> summonDic = new();
+    private Dictionary<string, SummonUI> _summonDic = new();
+    private bool _daddyDisabled;
 
     private void Awake()
     {
-        summonDic = new();
+        _summonDic = new();
         foreach (var summon in summons)
         {
             var summonUI = Instantiate(summonUIPrefab, summonDaddy.transform);
             summonUI.gameObject.SetActive(false);
             summonUI.SetSummon(summon);
-            summonDic.Add(summon.SummonName, summonUI);
+            _summonDic.Add(summon.SummonName, summonUI);
         }
     }
 
+    private void Start()
+        => SetDaddyStatus();
+ 
 
     private bool _isActive;
 
     private void Update()
     {
+        if (_daddyDisabled)
+            return;
+
         if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
             if (_isActive)
                 Message.Publish(new HideSummonMenu());
@@ -40,8 +48,8 @@ public class SummonMenu : OnMessage<GameStateChanged, HideSummonMenu, ShowSummon
     {
         foreach (var thing in thingsToDisable)
             thing.SetActive(false);
-        foreach (var summon in CurrentGameState.GameState.SummonNames.Where(x => summonDic.ContainsKey(x)))
-            summonDic[summon].gameObject.SetActive(true);
+        foreach (var summon in CurrentGameState.GameState.SummonNames.Where(x => _summonDic.ContainsKey(x)))
+            _summonDic[summon].gameObject.SetActive(true);
 
         _isActive = true;
     }
@@ -50,7 +58,7 @@ public class SummonMenu : OnMessage<GameStateChanged, HideSummonMenu, ShowSummon
     {
         foreach (var thing in thingsToDisable)
             thing.SetActive(true);
-        foreach (var summon in summonDic)
+        foreach (var summon in _summonDic)
             summon.Value.gameObject.SetActive(false);
 
         _isActive = false;
@@ -60,7 +68,27 @@ public class SummonMenu : OnMessage<GameStateChanged, HideSummonMenu, ShowSummon
     protected override void Execute(GameStateChanged msg)
     {
         if (_isActive)
+        {
             ShowActiveSummons();
+            return;
+        }
+        SetDaddyStatus();
+    }
+
+    private void SetDaddyStatus()
+    {
+        if (CurrentGameState.GameState.SummonNames.IsNullOrEmpty())
+        {
+            foreach (var thing in thingsToDisable)
+                thing.SetActive(false);
+            _daddyDisabled = true;
+            return;
+        }
+
+        foreach (var thing in thingsToDisable)
+            thing.SetActive(true);
+
+        _daddyDisabled = false;
     }
 
     protected override void Execute(HideSummonMenu msg)
