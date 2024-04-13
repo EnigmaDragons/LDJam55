33f281e1258c9f28dcbeb479db3894443e213941
SilasReinagel <silas.reinagel@gmail.com>
2024-04-13 02:57:21 -0500

    Coding: When Treasure is Stolen, Win Game. Closes #19

---
+++ src/LDJam55/Assets/Scripts/EventPublisher.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class EventPublisher : ScriptableObject
+{
+    public void WinGame() => Message.Publish(new GameWon());
+    public void LostGame() => Message.Publish(new GameOver());
+}
