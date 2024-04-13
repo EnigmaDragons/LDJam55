bbfa6ba39cb222cfdf667880a496a3671fefd407
SilasReinagel <silas.reinagel@gmail.com>
2024-04-13 16:57:12 -0500

    Design: Final Treasure is Trigger Enter. Closes #130

---
+++ src/LDJam55/Assets/Scripts/Gameplay/OnPlayerTriggerEnter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEnterEvent : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onTriggerEnter;
+    [SerializeField] private UnityEvent delayedEvent;
+    [SerializeField] private float delaySeconds = 1.5f;
+    [SerializeField] private bool canRetrigger = false;
+
+    private bool _triggered;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_triggered)
+            return;
+        
+        Log.Info("Trigger Enter", this);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!canRetrigger)
+                _triggered = true;
+            onTriggerEnter.Invoke();
+            this.ExecuteAfterDelay(() => delayedEvent.Invoke(), delaySeconds);
+        }
+    }
+}
