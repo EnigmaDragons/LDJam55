a0c8962337d0ccf92df7bd73c7d13f8cb9072c5d
SilasReinagel <silas.reinagel@gmail.com>
2024-04-13 10:52:42 -0500

    Coding: Created Triggerable Script. Closes #90

---
+++ src/LDJam55/Assets/Scripts/Gameplay/Triggerable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Triggerable : MonoBehaviour
+{
+    [SerializeField] private bool isTriggered;
+    [SerializeField] private bool canBeUntriggered;
+    [SerializeField] private bool startsTriggered;
+    [SerializeField] private UnityEvent onTrigger;
+    
+    public bool IsTriggered => isTriggered;
+
+    private void Start()
+    {
+        if (startsTriggered)
+            Trigger();
+    }
+    
+    public void Trigger()
+    {
+        isTriggered = true;
+        onTrigger.Invoke();
+    }
+
+    public void UnTrigger()
+    {
+        if (!canBeUntriggered)
+            return;
+        
+        isTriggered = false;
+    }
+}
