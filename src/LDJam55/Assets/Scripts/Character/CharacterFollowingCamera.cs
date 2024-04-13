34e1f8e59328e7dff62b3bf03360b16e9e46f578
Noah Reinagel <noah.r9@gmail.com>
2024-04-12 21:08:32 -0700

    added boundaries to the follow camera

--- src/LDJam55/Assets/Scripts/Character/CharacterFollowingCamera.cs
+++ src/LDJam55/Assets/Scripts/Character/CharacterFollowingCamera.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Character
 {
     public class CharacterFollowingCamera : MonoBehaviour
     {
+        [SerializeField] private float xMin;
+        [SerializeField] private float xMax;
+        [SerializeField] private float zMin;
+        [SerializeField] private float zMax;
+
         private GameObject player;
         
         private void OnEnable()
@@ -13,7 +19,7 @@ namespace Character
 
         public void Update()
         {
-            transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+            transform.position = new Vector3(Math.Clamp(player.transform.position.x, xMin, xMax), transform.position.y, Math.Clamp(player.transform.position.z, zMin, zMax));
         }
     }
 }
