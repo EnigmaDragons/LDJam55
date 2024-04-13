c5574982f1b79eef0e679327a118aa80360f403b
Noah Reinagel <noah.r9@gmail.com>
2024-04-12 20:52:22 -0700

    added player following camera

---
+++ src/LDJam55/Assets/Scripts/Character/CharacterFollowingCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Character
+{
+    public class CharacterFollowingCamera : MonoBehaviour
+    {
+        private GameObject player;
+        
+        private void OnEnable()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        public void Update()
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        }
+    }
+}
