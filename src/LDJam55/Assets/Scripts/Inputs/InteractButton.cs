6b286ff2ead5b4b095bf02e864bbf8be0cdf4cc3
SilasReinagel <silas.reinagel@gmail.com>
2024-04-13 02:11:26 -0500

    Coding: Bound Interace Button. Closes #68

---
+++ src/LDJam55/Assets/Scripts/Inputs/InteractButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Input = UnityEngine.Input;
+
+public static class InteractButton
+{
+    private const KeyCode InteractKey = KeyCode.F;
+
+    public static bool IsDown()
+    {
+        return Input.GetKeyDown(InteractKey);
+    }
+}
