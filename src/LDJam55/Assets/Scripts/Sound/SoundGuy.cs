3b977bc4c7a098a058001d4322fc1ca272c43870
SilasReinagel <silas.reinagel@gmail.com>
2024-04-14 08:47:16 -0500

    Started Sound Guy Script

---
+++ src/LDJam55/Assets/Scripts/Sound/SoundGuy.cs
@@ -0,0 +1,33 @@
+﻿using FMODUnity;
+using UnityEngine;
+
+public class SoundGuy : MonoBehaviour
+{
+    public EventReference summonLoopSound;
+    public EventReference summonKeyPressed;
+
+    private bool _isSummoning;
+    
+    private void OnEnable()
+    {
+        Message.Subscribe<SummonBegin>(_ => OnSummonStarted(), this);
+        Message.Subscribe<SummonFailed>(_ => OnSummonFailed(), this);
+    }
+
+    private void OnDisable()
+    {
+        Message.Unsubscribe(this);
+    }
+
+    private void OnSummonStarted()
+    {
+        _isSummoning = true;
+        // Code to start the Summon Loop Sound
+        // Code to trigger any Summon start oneshots
+    }
+
+    private void OnSummonFailed()
+    {
+        
+    }
+}
