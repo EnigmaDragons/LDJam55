853a59e0cae7a364536da688c33922ed2b9a4124
nightonmars <139680225+nightonmars@users.noreply.github.com>
2024-04-14 15:55:27 +0100

    Pressure switch SFX

---
+++ src/LDJam55/Assets/SwitchWeigthSFX.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class SwitchWeigthSFX : MonoBehaviour
+{
+
+    public void PlaySwitchSFX()
+    {
+        RuntimeManager.PlayOneShot("event:/SFX/WeightSwitch");
+    }
+}
