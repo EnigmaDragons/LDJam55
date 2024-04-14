249f72b58fec500ade49063d4baa0c747e705baa
Noah Reinagel <noah.r9@gmail.com>
2024-04-13 17:16:56 -0700

    made torches float and made torches use a different mechanism of triggering fire things

--- src/LDJam55/Assets/Scripts/Gameplay/TorchTrigger.cs
+++ src/LDJam55/Assets/Scripts/Gameplay/TorchTrigger.cs
@@ -1,15 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
 public class TorchTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject torch;
+    [SerializeField] private float range;
     
-    private void OnTriggerEnter(Collider other)
+    private void OnEnable()
     {
-        if (other.gameObject.CompareTag("Flammable"))
-        {
-            other.gameObject.GetComponent<Triggerable>().Trigger();
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
+        var flammableColliders = hitColliders.Where(x => x.gameObject.CompareTag("Flammable"));
+        foreach (var collider in flammableColliders)
+            collider.gameObject.GetComponent<Triggerable>().Trigger();
+        if (flammableColliders.Any())
             Destroy(torch);
-        }
     }
 }
