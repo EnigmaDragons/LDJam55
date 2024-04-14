d7882e1ca7fa2a978d123d1d282a48ff50fd84ac
nightonmars <139680225+nightonmars@users.noreply.github.com>
2024-04-14 21:40:22 +0100

    fmod stuff

--- src/LDJam55/Assets/Scripts/Gameplay/Door.cs
+++ src/LDJam55/Assets/Scripts/Gameplay/Door.cs
@@ -1,5 +1,7 @@
 ﻿using DG.Tweening;
 using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
 
 public class Door : MonoBehaviour
 {
@@ -10,13 +12,22 @@ public class Door : MonoBehaviour
     [SerializeField] private bool startsOpens = false;
     [SerializeField] private Transform doorScaleTarget;
     [SerializeField] private float animDurationSeconds = 1.5f;
+    public EventReference doorSuccesSFXRef;
 
     public bool IsOpen => isOpen;
+    private bool stopSoundAtStart;
     
     private void Awake()
     {
+        stopSoundAtStart = true;
         if(startsOpens)
             Open();
+    }
+
+    private void Start()
+    {
+        stopSoundAtStart = false;
+        Debug.Log(stopSoundAtStart);
     }
 
     public void Open()
@@ -28,6 +39,12 @@ public class Door : MonoBehaviour
         if (scaleForOpen)
             doorScaleTarget.DOScale(scaleWhenOpen, animDurationSeconds);
         doorScaleTarget.DOLocalMoveY(-0.1f, animDurationSeconds);
+        if(!stopSoundAtStart)
+        {
+            RuntimeManager.PlayOneShot(doorSuccesSFXRef);
+            Debug.Log("sound should play");
+        }
+
     }
 
     public void Close()
