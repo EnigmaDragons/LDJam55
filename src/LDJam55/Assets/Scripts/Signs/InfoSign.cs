d0aa7d728db94620fe4e3a1e7bab1a8762fd43d5
Daniel Xie <danielxie@Daniels-MacBook-Pro.local>
2024-04-13 22:07:18 -0700

    Add InfoSigns with dialog to scene

--- src/LDJam55/Assets/Scripts/Signs/InfoSign.cs
+++ src/LDJam55/Assets/Scripts/Signs/InfoSign.cs
@@ -28,13 +28,16 @@ public class InfoSign : MonoBehaviour
 
     public void ShowInfo() {
         if (isAncientLanguage) {
+            Debug.Log("Showing Ancient Language Sign Text");
             Message.Publish(new ShowInfoSignDialog("This sign bears inscriptions in an ancient, mystical language. Its meaning eludes you."));
         } else {
+            Debug.Log("Showing Sign Info: " + signText);
             Message.Publish(new ShowInfoSignDialog(signText));
         }
     }
 
     public void HideInfo() {
+        Debug.Log("Hiding Sign Info");
         Message.Publish(new HideInfoSignDialog());
     }
 
--- src/LDJam55/Assets/Scripts/Signs/InfoSignDialog.cs
+++ src/LDJam55/Assets/Scripts/Signs/InfoSignDialog.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class InfoSignDialog : OnMessage<ShowInfoSignDialog, HideInfoSignDialog>
 {
-    [SerializeField] private GameObject dialogPanel = null;
-    [SerializeField] private TMPro.TextMeshProUGUI text = null;
+  
+    [SerializeField] private GameObject panel;
+    [SerializeField] private TMPro.TextMeshProUGUI text;
 
     protected override void Execute(ShowInfoSignDialog msg) {
         text.text = msg.text;
-        dialogPanel.SetActive(true);
+        panel.SetActive(true);
+        text.gameObject.SetActive(true);
     }
 
     protected override void Execute(HideInfoSignDialog msg) {
-        dialogPanel.SetActive(false);
+        panel.SetActive(false);
+        text.gameObject.SetActive(false);
     }
 }
