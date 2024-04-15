972f86d7f59ddd2071bdb61a49a28d83ed2aec6a
SilasReinagel <silas.reinagel@gmail.com>
2024-04-15 13:45:29 -0500

    Design: Added Final Puzzle Room. Closes #284

---
+++ src/LDJam55/Assets/Scripts/Gameplay/ShouldBeFacing.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ShouldBeFacing : ConstraintBase
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private float yRotation;
+    
+    public override bool IsSatisfied => Mathf.Abs(target.transform.eulerAngles.y - yRotation) <= 1;
+}
