d95db47b6ec2112c213480a7bf5683c7d6f47602
Noah Reinagel <noah.r9@gmail.com>
2024-04-15 11:56:15 -0700

    treasure cutscene and player is a little more snug

---
+++ src/LDJam55/Assets/Scripts/Story/OnTreasurePlayCutscene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class OnTreasurePlayCutscene : OnMessage<TreasureAcquired>
+{
+    [SerializeField] private Cutscene cutscene;
+    
+    protected override void Execute(TreasureAcquired msg)
+    {
+        Message.Publish(new PlayCutscene(cutscene));
+    }
+}
