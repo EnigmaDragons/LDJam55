5d96da226dcf4310e9ba4b3d84fc4329e377f7c4
Noah Reinagel <noah.r9@gmail.com>
2024-04-15 12:22:00 -0700

    room o lets you enter it

--- src/LDJam55/Assets/Scripts/Gameplay/GameOverHandler.cs
+++ src/LDJam55/Assets/Scripts/Gameplay/GameOverHandler.cs
@@ -1,6 +1,24 @@
 ﻿
-public class GameOverHandler : OnMessage<GameWon, GameOver>
+using Story;
+using UnityEngine;
+
+public class GameOverHandler : OnMessage<GameWon, GameOver, CutsceneFinished, TreasureAcquired>
 {
+    [SerializeField] private Cutscene win;
+    [SerializeField] private Cutscene lostBeforeTreasure;
+    [SerializeField] private Cutscene lostAfterTreasure;
+
+    private bool _treasureAquired;
+    private bool _navigateToCredits;
+    private bool _navigateToGameOver;
+
+    private void Start()
+    {
+        _treasureAquired = false;
+        _navigateToCredits = false;
+        _navigateToGameOver = false;
+    } 
+    
     protected override void Execute(GameWon msg)
     {
         Message.Publish(new NavigateToSceneRequested("CreditsScene"));
@@ -8,6 +26,17 @@ public class GameOverHandler : OnMessage<GameWon, GameOver>
 
     protected override void Execute(GameOver msg)
     {
+        
         Message.Publish(new NavigateToSceneRequested("GameOverScene"));
+    }
+
+    protected override void Execute(CutsceneFinished msg)
+    {
+        
+    }
+
+    protected override void Execute(TreasureAcquired msg)
+    {
+        _treasureAquired = true;
     }
 }
